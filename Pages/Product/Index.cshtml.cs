ed239269231a2aec7073b9879bd52de31916001f
Ethan Tao <ke5h@ntao.ca>
2022-03-29 14:15:29 -0400

    product page can only edit with use login

--- Pages/Product/Index.cshtml.cs
+++ Pages/Product/Index.cshtml.cs
@@ -19,7 +19,7 @@ namespace ProductModel
             _context = context;
         }
 
-        public IList<Product> Product { get;set; }
+        public IList<Product> Product { get; set; }
 
         public async Task OnGetAsync()
         {
