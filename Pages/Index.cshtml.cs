b7a20c88a27e50b1e95dc7a446bd713c1c2d9364
Ethan Tao <ke5h@ntao.ca>
2022-03-29 14:16:12 -0400

    redirect to product page after user login

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace lab3.Pages;
 
@@ -12,8 +15,30 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    public string? UserEmail { get; set; }
+
     public void OnGet()
     {
+        if (User.Identity != null)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            if (claimsIdentity.IsAuthenticated)
+            {
+                var email = claimsIdentity.FindFirst(ClaimTypes.Email);
+                if (email != null)
+                {
+                    UserEmail = email.Value;
+                    // _logger.Log(LogLevel.Information, email.Value);
+                }
+                // foreach (var cl in claimsIdentity.Claims)
+                // {
+                //     _logger.Log(LogLevel.Information, $"{cl.Type}:{cl.Value}");
+
+                // }
+
+                Response.Redirect("/Product/Index");
+            }
+        }
 
     }
 }
