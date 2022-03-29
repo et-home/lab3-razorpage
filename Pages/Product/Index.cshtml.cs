9854b7aa26fee90fb5cee6ac809e33fc2b381c43
Ethan Tao <ke5h@ntao.ca>
2022-03-29 14:31:24 -0400

    show login user email on product page

--- Pages/Product/Index.cshtml.cs
+++ Pages/Product/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using lab3.Models;
+using System.Security.Claims;
 
 namespace ProductModel
 {
@@ -20,10 +21,25 @@ namespace ProductModel
         }
 
         public IList<Product> Product { get; set; }
+        public string? UserEmail { get; set; }
 
         public async Task OnGetAsync()
         {
             Product = await _context.Product.ToListAsync();
+
+            if (User.Identity != null)
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                if (claimsIdentity.IsAuthenticated)
+                {
+                    var email = claimsIdentity.FindFirst(ClaimTypes.Email);
+                    if (email != null)
+                    {
+                        UserEmail = email.Value;
+                    }
+                }
+            }
+
         }
     }
 }
