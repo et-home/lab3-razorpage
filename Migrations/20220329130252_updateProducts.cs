717d0f63bcc0dcc74ba07d5c1bc6e7af105a22ed
Ethan Tao <ke5h@ntao.ca>
2022-03-29 11:26:51 -0400

    product annotation change

---
+++ Migrations/20220329130252_updateProducts.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab3.Migrations
+{
+    public partial class updateProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Province",
+                table: "User",
+                type: "varchar(2)",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(26)",
+                oldMaxLength: 26)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Product",
+                type: "decimal(7,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Province",
+                table: "User",
+                type: "varchar(26)",
+                maxLength: 26,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(2)",
+                oldMaxLength: 2)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Product",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(7,2)");
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -13,6 +13,9 @@ public class Product
     [Display(Name = "Book")]
     public string? ImageLink { get; set; }
 
+    [Range(0, 20000)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(7,2)")]
     public decimal Price { get; set; }
 
     public int Quantity { get; set; }
